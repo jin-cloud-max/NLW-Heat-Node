ea4fb2b48b321fe8e1719bbef6d905f3e5b42b58
Jin B. Oliveira <jinoliveira74@gmail.com>
2021-10-19 18:31:47 -0300

    chore: cadastro de mesnagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 import { AuthenticateUserController } from './controller/AuthenticateUserController'
+import { CreateMessageController } from './controller/CreateMessageController'
+import { ensureAuthenticate } from './middleware/ensureAuthenticate'
 
 const router = Router()
 
 router.post("/authenticate", new AuthenticateUserController().handle)
+router.post("/messages", ensureAuthenticate, new CreateMessageController().handle)
 
 export { router }
